af5fbeefe3c85b3ea6397918d665678974270bc2
jas-writes-code <virgil@virgildoesthings.com>
2025-10-28 12:30:35 +0100

    fix command processing

--- bot.py
+++ bot.py
@@ -165,15 +165,16 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if not message.author.bot or message.author.system:
-        parts = message.content.strip().split()
-        cmd = parts[0].lstrip("!")
-        args = parts[1:]
-        if cmd in config["commands"]:
-            action_name = config["commands"][cmd]
-            func = globals().get(action_name)
-            if callable(func):
-                await func(message, args)
-            else:
-                print(f"No function defined for action '{action_name}'")
+        if message.content.startswith("!"):
+            parts = message.content.strip().split()
+            cmd = parts[0].lstrip("!")
+            args = parts[1:]
+            if cmd in config["commands"]:
+                action_name = config["commands"][cmd]
+                func = globals().get(action_name)
+                if callable(func):
+                    await func(message, args)
+                else:
+                    print(f"No function defined for action '{action_name}'")
 
 client.run(str(key))
